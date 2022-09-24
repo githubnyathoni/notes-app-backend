b39c5881f8ce97c699b9bfa03772c96416a85539
Ahmad Fathoni <ahmad.fathoni50@gmail.com>
2022-09-24 23:46:12 +0700

    add plugin users

---
+++ src/api/users/handler.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-underscore-dangle */
+class UsersHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    this.postUserHandler = this.postUserHandler.bind(this);
+    this.getUserByIdHandler = this.getUserByIdHandler.bind(this);
+  }
+
+  async postUserHandler({ payload }, h) {
+    this._validator.validateUserPayload(payload);
+
+    const userId = await this._service.addUser(payload);
+
+    const response = h.response({
+      status: 'success',
+      message: 'User berhasil ditambahkan',
+      data: {
+        userId,
+      },
+    });
+
+    response.code(201);
+    return response;
+  }
+
+  async getUserByIdHandler({ params: { id } }) {
+    const user = await this._service.getUserById(id);
+
+    return {
+      status: 'success',
+      data: {
+        user,
+      },
+    };
+  }
+}
+
+module.exports = UsersHandler;
